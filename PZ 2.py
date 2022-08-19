50f92f62c4352712939d3e5aa17e8aaf98fe8a01
MAX <ankin.maks@mail.ru>
2022-08-19 18:49:04 +0300

    pz2

---
+++ PZ 2.py
@@ -0,0 +1,11 @@
+# # # ZADANIE 1
+# # print("НАПИШИТЕ ЧИСЛО ЧТОБЫ УВИДЕТЬ СУММУ")
+# # x= input()
+# # print(sum(map(int,str(x))))
+# # # ZADANIE 2
+# # import functools
+# # import math
+# # y=input()
+# # print ( math.prod(map(int,str(y))))
+
+
